e44c78ce014f46eee56ee4b729461f61e4f97220
Chakravarti Raghavan <rchak1@aol.com>
2021-04-20 01:20:38 -0700

    jupyterHWs upto April 19

---
+++ Jupyter-Workspace/hello_variable_world.py
@@ -0,0 +1,44 @@
+# Percent Increase Bonus Activity
+
+Original_price = 198.87
+Current_price = 254.32
+
+
+
+
+# Formulas
+# Increase = Current Price - Original Price
+
+increase = Current_price - Original_price
+
+# Percent Increase = Increase / Original x 100
+percent_increase = increase / Original_price * 100
+
+
+# Create float variable for original_price
+
+
+# Create float variable for current_price
+
+
+# Calculate difference between current_price and original_price
+
+
+# Calculate percent_increase
+
+
+# Print original_price
+print(f"Apple's original stock price was ${Original_price}")
+
+# Print current_price
+print(f"Apples current stock price is ${Current_price}")
+
+# Print percent_increase
+print(percent_increase)
+
+width = 10
+precision = 1
+#result = {percent_increase:{width}.{precision}}
+#print(result)
+
+
